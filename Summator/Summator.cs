0b02ed4b3d9a66764abb716288bb549e80281777
Dimo Mitev <dimosoftuni@gmail.com>
2024-02-29 20:55:27 +0200

    Add new method

--- Summator/Summator.cs
+++ Summator/Summator.cs
@@ -33,5 +33,18 @@ namespace Summator
             // return sum
             return sum / arr.Length;
         }
+
+        public static long findMax(int[] arr)
+        {
+            long sum = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+
+            // return sum
+            return sum;
+        }
     }
 }
